bbe4f8441a05e3a983a1b65dd8d08969b79cae0d
haott <haot253@gmailcom>
2022-10-06 22:59:20 +0700

    homework1

--- rn25-haott/src/App.tsx
+++ rn25-haott/src/App.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import img from "./image/traffic.jpeg";
+import img from "./image/traffic2.jpg";
 
 function App() {
   return (
     <>
       <div id="#app">
-        <div className="header">
-          <div className="header__heading">The Pulpit Rock</div>
+        <div className="header bg-ccc h-100">
+          <div className="header__heading text-white">The Pulpit Rock</div>
         </div>
-        <div className="content row">
-          <div className="content1 col-3">
+        <div className="content row p-30">
+          <div className="content1 col-12 col-md-3">
             <ul className="content1__list">
               <li className="content1__item">The Drive</li>
               <li className="content1__item">The Walk</li>
@@ -19,37 +19,41 @@ function App() {
               <li className="content1__item">The End</li>
             </ul>
           </div>
-          <div className="content2 col-6">
-            <div className="content2__heading">The Walk</div>
-            <div className="content2__desc">
+          <div className="content2 col-md-6">
+            <div className="content__heading">The Walk</div>
+            <div className="content2__desc pb-30">
               The walk to the Pulpit Rock will take you approximately two hours,
               give or take an hour depending on the weather conditions and your
               physical shape.
             </div>
-            <img className="content2__img" src={img} alt="" />
+            <img className="content2__img h-75" src={img} alt="" />
           </div>
-          <div className="content3 col-3">
-            <div className="content3__name">
-              <div className="content3__heading">What?</div>
+          <div className="content3 col-12 col-md-3 h-75">
+            <div className="content3__name pb-30">
+              <div className="content__heading ">What?</div>
               <div className="content3__desc">
                 The Pulpit Rock is a part of a mountain that looks like a
                 pulpit.
               </div>
             </div>
-            <div className="content3__name">
-              <div className="content3__heading">Where?</div>
+            <div className="content3__name pb-30">
+              <div className="content__heading">Where?</div>
               <div className="content3__desc">
                 The Pulpit Rock is in Norway.
               </div>
             </div>
-            <div className="content3__name">
-              <div className="content3__heading">Price?</div>
+            <div className="content3__name pb-30">
+              <div className="content__heading">Price?</div>
               <div className="content3__desc">The walk is free!</div>
             </div>
           </div>
         </div>
-        <div className="footer">
-          <p className="footer__desc"></p>
+        <div className="footer bg-ccc text-white text-center h-100">
+          <p className="footer__desc">
+            This web page is a part of a demonstration of fluid web design made
+            by www.w3schools.com. Resize the browser window to see the content
+            response to the resizing.
+          </p>
         </div>
       </div>
     </>
